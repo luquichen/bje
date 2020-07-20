45a22e7136ee7fb4eb214b0020516e59af20b954
lucas <lucasremolacha@gmail.com>
2020-07-20 17:08:48 -0300

    creacion de la clase cartas

--- src/modelo/CartasJuego.java
+++ src/modelo/CartasJuego.java
@@ -2,6 +2,28 @@ package modelo;
 
 public class CartasJuego {
 
+//ATRIBUTOS DE UNA CARTA
+    private String palo;
+    private String valorMostrar;
+    private int valorReal;
 
+//CONSTRUCTOR
+    public CartasJuego (String palo, String valorMostrar, int valorReal){
+        this.palo = palo;
+        this.valorMostrar = valorMostrar;
+        this.valorReal = valorReal;
+    }
+
+//METODOS-   {en este caso todos los metodos de la carta son geter}
+
+    public String getPalo(){
+        return this.palo;
+    }
+    public String getValorMostrar(){
+        return this.valorMostrar;
+    }
+    public int getValorReal(){
+        return this.valorReal;
+    }
 
 }
